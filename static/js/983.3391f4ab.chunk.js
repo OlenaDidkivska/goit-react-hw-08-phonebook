bcf0eaa7451236c1871a718f6e65af4adbcbeb6c
OlenaDidkivska <99613214+OlenaDidkivska@users.noreply.github.com>
2022-12-10 22:27:52 +0000

    Deploying to gh-pages from @ OlenaDidkivska/goit-react-hw-08-phonebook@4d5121ee60b8d7d15b3d90367b9ff5a593dfca7f 🚀

---
+++ static/js/983.3391f4ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,n,o){o.r(n);var r=o(2606),l=o(7005),t=o(3329);n.default=function(){return(0,t.jsxs)(r.xo,{children:[(0,t.jsxs)("h1",{children:["PhoneBook welcome page"," ",(0,t.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]}),(0,t.jsxs)("div",{children:["Go to ",(0,t.jsx)(l.w,{to:"/login",children:"LogIn"})]})]})}}}]);
+//# sourceMappingURL=983.3391f4ab.chunk.js.map
